f69d9d8a173ac39374cb0d109c4b20df0be3119e
Kailash Lama <kailashlama@Kailashs-MacBook-Pro-2.local>
2025-11-06 08:28:20 +0545

    4  update

--- myScript.js
+++ myScript.js
@@ -1,3 +1,20 @@
-function myFunction() {
-  document.getElementById("myScript.js").innerHTML = "Paragraph changed.";
+function changeFontSize() {
+    const box = document.querySelector('.box1');
+    box.style.fontSize = '24px';
+}
+
+function changeStyle() {
+    const box = document.querySelector('.box3');
+    box.style.fontStyle = 'italic';
+}
+
+function changeImage() {
+    const box = document.querySelector('.box5');
+    box.style.backgroundImage = 'url(tiger.jpg)';
+    box.style.backgroundSize = 'cover';
+}
+
+function addText() {
+    const box = document.querySelector('.box7');
+    box.textContent = 'New Text!';
 }
